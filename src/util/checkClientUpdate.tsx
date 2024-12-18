f7a9a084222559a29d53378662659abae8b363bb
loudpatches <loudpatches@that-place>
2024-12-18 17:02:14 -0600

    Account for V tags.  Shouldn't matter going forward but meh.

--- src/util/checkClientUpdate.tsx
+++ src/util/checkClientUpdate.tsx
@@ -11,7 +11,7 @@ const checkClientUpdate$ = () =>
                   }: {response: {tag_name: string;
                                  assets: { name: string; browser_download_url: string }[];
                                 };
-                     }) => {const result = compare(version, tag_name);
+                     }) => {const result = compare(version, tag_name.replace("V",""));
                             if (result < 0) {
                                 const exeUrl = assets.find((asset) => asset.name.endsWith('.exe'));
                                 return exeUrl?.browser_download_url ?? null;}
